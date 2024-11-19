06b9187d8680ca23b92e1a16cb6b1bde480740d7
kwanghe <bara3013@gmail.com>
2024-11-19 12:50:06 +0900

    3

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BASE_URL: 'https://kwanghe.github.io/digi_yunri_test1/'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+} 
